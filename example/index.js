0829b58ed7539091682713d6dea0457516dd8fc1
Michael Swiss <mswiss@discovery.com>
2019-08-13 15:15:10 -0400

    demo of driver loading already loaded app, waiting for browse and navigating to unlocked show

--- example/index.js
+++ example/index.js
@@ -11,34 +11,25 @@ const configJson = require('./driver.config.json');
         await driver.wait(until.documentLoaded(), 15000);
         console.log("document loaded ");
         await driver.wait(until.elementLocated(By.name('BROWSE')), 20000);
-        // await setTimeout(function() {
-        //     driver.sendKeys([
-        //         Key.DOWN,
-        //         Key.DOWN,
-        //         Key.DOWN,
-        //         Key.OK
-        //     ]);
-        // }, 12000);
-        console.log("sent keys ");
+        console.log("sent keys");
         await driver.sendKeys([
             Key.DOWN,
             Key.DOWN,
             Key.DOWN,
             Key.OK
         ]);
-        // await driver.wait(until.elementLocated(By.text('Sign In To Watch')), 20000);
-        // await driver.sendKeys([
-        //     Key.DOWN,
-        //     Key.DOWN,
-        //     Key.OK
-        // ]);
+        await driver.wait(until.elementLocated(By.text('Til Death Do Us Part')), 20000);
+        console.log("text found ");
+        await driver.sendKeys([
+            Key.DOWN,
+            Key.DOWN,
+            Key.OK
+        ]);
         // await driver.wait(until.elementLocated(By.id('exampleVector2DAnimation')), 2000);
         const screenshot = await driver.takeScreenshot();
         const nowSeconds = Math.round(Date.now() / 1000);
         fs.writeFileSync(`screenshots/screenshot_${nowSeconds}.jpg`, screenshot);
-        // await driver.sendKeys([
-        //     Key.BACK
-        // ]);
+
     } catch(err) {
         console.error(err);
     } finally {
